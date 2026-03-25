b3dd871da5542929b8edd7246c35f654af64d4c5
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-25 18:06:56 -0400

    Started on Step 1 -- added recursive binary searach function with both targets and started to add iterative binary search.

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -1,1 +1,25 @@
+from search_algorithms import recursive_binary_search, iterative_binary_search, sequential_search
 
+#Test list:
+
+arr = [3, 5, 8, 12, 14, 18, 21]
+arr.sort()
+target1 = 12 # Present
+target2 = 9 # Not present
+
+#Call each search function with both targets. Print results:
+
+#Recursive Binary Search
+
+#Target 1
+index = recursive_binary_search(arr, target1, 0, len(arr) - 1)
+print(f"{target1} found at index {index}" if index != -1 else
+f"{target1} not found")
+
+#Target 2
+index = recursive_binary_search(arr, target2, 0, len(arr) - 1)
+print(f"{target2} found at index {index}" if index != -1 else
+f"{target2} not found")
+
+#Iterative Binary Search
+index2 = iterative_binary_search(arr, target)
