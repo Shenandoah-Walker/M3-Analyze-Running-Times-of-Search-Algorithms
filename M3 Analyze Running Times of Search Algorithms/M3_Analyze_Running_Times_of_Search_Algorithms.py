ebbb9bb43cd533f10a5dac7570ec5295fe16a0c3
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-26 10:56:35 -0400

    Added sorting

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -1,6 +1,13 @@
 #Random Numbers branch
-
+from search_algorithms import sequential_search, binary_search, interpolation_search
 import random
 
 arr = [random.randint(1, 100) for _ in range(20)]
+arr = sorted([random.randint(1, 1000000) for _ in range(N)])
 target = random.choice(arr) if random.random() < 0.5 else 999 # 50% chance of failure
+
+
+
+
+
+
