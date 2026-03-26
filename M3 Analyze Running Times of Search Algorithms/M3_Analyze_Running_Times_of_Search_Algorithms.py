18e8c5887b144912317e488124d29c294d4208f3
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-26 16:20:07 -0400

    Added Recursive Binary Search

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -7,6 +7,11 @@ arr = sorted([random.randint(1, 1000000) for _ in range(N)])
 target = random.choice(arr) if random.random() < 0.5 else 999 # 50% chance of failure
 
 
+#Recursive Binary Search
+index = recursive_binary_search(arr, target, 0, len(arr) - 1)
+print(f"{target} found at index {index}" if index != -1 else
+f"{target} not found")
+
 
 
 
