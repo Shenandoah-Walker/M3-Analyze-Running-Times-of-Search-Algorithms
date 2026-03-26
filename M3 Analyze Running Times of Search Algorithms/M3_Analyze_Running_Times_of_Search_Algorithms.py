89bff7f8e58a4ae699d0a1638c4247da0b6680e1
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-26 16:26:24 -0400

    Added Iterative Binary Search and Fixed Inorrect import on Line 1

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -1,5 +1,5 @@
 #Random Numbers branch
-from search_algorithms import sequential_search, binary_search, interpolation_search
+from search_algorithms import recursive_binary_search, iterative_binary_search, sequential_binary_search
 import random
 
 arr = [random.randint(1, 100) for _ in range(20)]
@@ -12,7 +12,10 @@ index = recursive_binary_search(arr, target, 0, len(arr) - 1)
 print(f"{target} found at index {index}" if index != -1 else
 f"{target} not found")
 
-
+#Iterative Binary Search
+index = iterative_binary_search(arr, target)
+print(f"{target} found at index {index}" if index != -1 else
+f"{target} not found")
 
 
 
