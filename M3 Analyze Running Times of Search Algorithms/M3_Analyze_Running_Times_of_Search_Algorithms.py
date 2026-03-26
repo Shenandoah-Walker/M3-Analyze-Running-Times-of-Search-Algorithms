bf0d95d05e32de53a933c46641dec052e88fae58
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-26 10:25:48 -0400

    Finished adding iterative binary search and added sequential search

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -16,10 +16,31 @@ index = recursive_binary_search(arr, target1, 0, len(arr) - 1)
 print(f"{target1} found at index {index}" if index != -1 else
 f"{target1} not found")
 
+## Repeat for other search methods and for target2
+#Note, remember that the call parameters for iterative_binary_search and
+#sequential_search are: search(arr, target)
+
 #Target 2
 index = recursive_binary_search(arr, target2, 0, len(arr) - 1)
 print(f"{target2} found at index {index}" if index != -1 else
 f"{target2} not found")
 
 #Iterative Binary Search
-index2 = iterative_binary_search(arr, target)
+
+index = iterative_binary_search(arr, target1)
+print(f"{target1} found at index {index}" if index != -1 else
+f"{target1} not found")
+
+index = iterative_binary_search(arr, target2)
+print(f"{target2} found at index {index}" if index != -1 else
+f"{target2} not found")
+
+#Sequential Search
+
+index = sequential_search(arr, target1)
+print(f"{target1} found at index {index}" if index != -1 else
+f"{target1} not found")
+
+index = sequential_search(arr, target2)
+print(f"{target2} found at index {index}" if index != -1 else
+f"{target2} not found")
