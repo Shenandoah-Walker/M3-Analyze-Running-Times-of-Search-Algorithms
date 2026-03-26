09c529b2a9f2d76a6c7bed4f0542e2d4000dd34a
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-03-26 10:32:11 -0400

    Created Random Numbers branch and began writing code for it.

--- M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
+++ M3 Analyze Running Times of Search Algorithms/M3_Analyze_Running_Times_of_Search_Algorithms.py
@@ -1,46 +1,6 @@
-from search_algorithms import recursive_binary_search, iterative_binary_search, sequential_search
+#Random Numbers branch
 
-#Test list:
+import random
 
-arr = [3, 5, 8, 12, 14, 18, 21]
-arr.sort()
-target1 = 12 # Present
-target2 = 9 # Not present
-
-#Call each search function with both targets. Print results:
-
-#Recursive Binary Search
-
-#Target 1
-index = recursive_binary_search(arr, target1, 0, len(arr) - 1)
-print(f"{target1} found at index {index}" if index != -1 else
-f"{target1} not found")
-
-## Repeat for other search methods and for target2
-#Note, remember that the call parameters for iterative_binary_search and
-#sequential_search are: search(arr, target)
-
-#Target 2
-index = recursive_binary_search(arr, target2, 0, len(arr) - 1)
-print(f"{target2} found at index {index}" if index != -1 else
-f"{target2} not found")
-
-#Iterative Binary Search
-
-index = iterative_binary_search(arr, target1)
-print(f"{target1} found at index {index}" if index != -1 else
-f"{target1} not found")
-
-index = iterative_binary_search(arr, target2)
-print(f"{target2} found at index {index}" if index != -1 else
-f"{target2} not found")
-
-#Sequential Search
-
-index = sequential_search(arr, target1)
-print(f"{target1} found at index {index}" if index != -1 else
-f"{target1} not found")
-
-index = sequential_search(arr, target2)
-print(f"{target2} found at index {index}" if index != -1 else
-f"{target2} not found")
+arr = [random.randint(1, 100) for _ in range(20)]
+target = random.choice(arr) if random.random() < 0.5 else 999 # 50% chance of failure
